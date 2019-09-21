9ca09b854d662a1ff89a2058799fd101e9e3e666
Rajaneesh Mutyalapati <rajaneesh.mutyalapati@gm.com>
2019-09-21 13:58:27 -0400

    working dummy code to generate item jsons for each entry.

--- data/rand_item_generator.py
+++ data/rand_item_generator.py
@@ -1,13 +1,40 @@
 import sys
+import json
+from random import seed
+from random import randint
 
+# seed random number generator
+seed(1)
 
 class rand_item_generator:
     
     def __init__(self):
-        __items_dict = {"Milk" : [2,1,0.5] ,
-        "Tea" : [] }
+        # The format "Item" : [Price,recyclable,co2 emissions]
+        self.__items_list = [
+        {"name" : "Milk",
+        'price': 2,
+        'recylable' : 1,
+        'co2_emissions' : 1} ,         
+        {"name" : "Tea",
+        'price': 4,
+        'recylable' : 1,
+        'co2_emissions' : 2},
+        {"name" : "Fruits",
+        'price': 3,
+        'recylable' : 1,
+        'co2_emissions' : 0.5}
+        ]
 
     def get_item(self):
+        max_rand_num = len(self.__items_list) - 1
+        rand_num = randint(0,max_rand_num)
+        return json.dumps(self.__items_list[rand_num])
 
 
 
+
+if __name__ == "__main__":
+    rd_gen = rand_item_generator() 
+    print(rd_gen.get_item())
+    print(rd_gen.get_item())
+    pass
